aecdd47f0f7d9d4f1954f3363fe413af3d1de27e
James Ferguson <jferguson2.718@gmail.com>
2013-06-09 20:22:26 -0400

    Made interval fn better

--- src/familiar/rangefns.clj
+++ src/familiar/rangefns.clj
@@ -2,15 +2,15 @@
   (or (true? x) (false? x)))
 
 (defn interval [b e]
-  "Returns true if a real number on interval [b,e]. Enter \"inf\"
+  "Returns true if a real number on interval [b,e]. Use nil
   instead of a number for infinite upper and/or lower bound."
   (fn [x]
     (try (+ x 0) (catch Exception e false))
-    (cond (and (= b "inf") (= e "inf"))
+    (cond (and (nil? b) (nil? e))
           true
-          (and (= b "inf") (<= x e))
+          (and (nil? b) (<= x e))
           true
-          (and (= e "inf") (>= x b))
+          (and (nil? e) (>= x b))
           true
           (and (>= x b) (<= x e))
           true
