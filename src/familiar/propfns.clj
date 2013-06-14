0173f4fd820378c90615687250fab5bb79bea9f3
James Ferguson <jferguson2.718@gmail.com>
2013-06-14 18:26:38 -0400

    Wrote an awful proposition function

--- src/familiar/propfns.clj
+++ src/familiar/propfns.clj
@@ -1,23 +1,36 @@
+(defn- inst-in-int [begin end instmap]
+  (->> instmap
+       (filter #(within? (interval (parse-date begin)
+                                   (parse-date end))
+                         (parse-date (key %))))
+       flatten
+       (apply assoc inst-map)))
+
 (defn val-prop-time [variable validator proportion rel-time]
   "True when variable passes validator for >= proportion of instances
-  over interval rel-time. Rel-time may be negative.
-  Examples:
-  'Exercised at least a little 6/7 days in the prior week'
-  (val-prop-time \"exercise\" (interval 1 nil) 6/7 (weeks -1))
+    over interval rel-time. Rel-time may be negative.
+    Examples:
+    'Exercised any amount for 6/7 days in the prior week'
+    (val-prop-time \"exercise\" (num-interval 1 nil) 6/7 (weeks -1))
 
-  'Mood was never 1 or 2 in prior month'
-  (val-prop-time \"mood\" #{1 2} 0 (months -1))
+    'Mood was never 1 or 2 in prior month'
+    (val-prop-time \"mood\" #{1 2} 0 (months -1))
 
-  'Used caffeine the next day'
-  (val-prop-time \"caffeine\" #{0} 0 (days 1))"
-  ; rel-time may be + or -
+    'Used caffeine the next day'
+    (val-prop-time \"caffeine\" #{0} 0 (days 1))"
   (fn [t]
     (let [times (assoc inst-map t :_,
                   (unparse-date (plus (parse-date t) rel-time)) :_)
           end   (ffirst times)
           begin (first (last times))]
       (->> (:instances ((str->key variable) @experiment))
-           (filter #(within? (interval (parse-date begin)
-                                       (parse-date end)) %))
-           ; ????
-         )))) 
+           (inst-in-int begin end)
+           vals
+           (group-by validator)
+           (group-by #(or (nil? (key %))
+                          (false? (key %))))
+           vals
+           (map (comp count flatten #(map second %)))
+           sort
+           (apply /)
+           (< proportion)))))
