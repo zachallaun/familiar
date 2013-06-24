517503c7c77e22aabccfd657f154e8cb3c0df85b
James Ferguson <jferguson2.718@gmail.com>
2013-06-24 16:20:37 -0400

    Time makes some sense now, adding data is un-broken

--- src/familiar/core.clj
+++ src/familiar/core.clj
@@ -24,28 +24,28 @@
                   :default false
                   :instances inst-map 
                   :unit "boolean"
-                  :time-res :day
+                  :time-res :date
                   :tags '(:food)}
      :outside    {:name "outside"
                   :validator '(num-interval 0 24) 
                   :default 1 
                   :unit "hours"
                   :instances inst-map
-                  :time-res :day
+                  :time-res :date
                   :tags '()}
      :exercise   {:name "exercise"
                   :validator #{0 1 2 3} 
                   :default 0
                   :unit "subjective strenuousness"
                   :instances inst-map
-                  :time-res :day
+                  :time-res :date
                   :tags '(:fitness)}
      :mood       {:name "mood"
                   :validator #{1 2 3 4 5}
                   :default 2
                   :unit "holistic mood rating"
                   :instances inst-map
-                  :time-res :day
+                  :time-res :date
                   :tags '()}}))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -97,19 +97,17 @@
      :tags - a sequence of strings with which to tag the variable"
   [variable validator default & opts]
   `(add-variable- ~variable '~validator ~default ~@opts))
-
 (defn- add-variable-
   [variable validator default
    & {:keys [expt time-res unit tags]
         :or {expt active-expt
-             time-res :day
+             time-res :date
              unit ""
              tags '()}}]
   (let [varkey (str->key variable)
-        valfn (eval validator)
         tags (map str->key tags)]
     (assert (nil? (varkey @expt)) "Variable already exists") 
-    (assert (valfn default) "Default not in range")
+    (assert ((eval validator) default) "Default fails validator")
     (swap! expt 
            assoc   
            varkey
@@ -122,43 +120,44 @@
             :tags tags})
     (display-vars)))
 
-;;;;
-(defn tag-var
-  "Adds tag to given variables in the current experiment."
-  [tag & variables]
-  (for [v variables]
-    (swap! active-expt 
-           update-in 
-           [(str->key v) :tags]
-           #(conj % (str->key tag)))))
-
-;;;; replace data within current time pixel 
 (defn add-datum
   "Adds a single instance of variable."
-  [variable value & {:keys [expt instant]
-                     :or {expt active-expt
-                          instant @active-time}}]
-  (assert ((eval (-> @expt 
-                     ((str->key variable))
-                     :validator))
-           value)
-          "Value not in range")
-  (swap! expt 
-         update-in 
-         [(str->key variable) :instances] 
-         #(assoc % instant value)))
+  [varname value & {:keys [expt instant]
+                      :or {expt active-expt
+                           instant @active-time}}]
+  (let [variable ((str->key varname) @expt)
+        instant (chop-time instant (:time-res variable))]
+    (assert ((eval (:validator variable)) value)
+            "Value fails validator")
+    (swap! expt 
+           update-in 
+           [(str->key varname) :instances] 
+           #(assoc % instant value))))
 
-;;;;
 (defn add-data 
-  "Adds instances of variables with values at active time.
+  "Adds instances of variables with values.
      Example:
-     (add-data \"mice\" 6 \"cats\" 2 \"dogs\" 0)"
-  [& coll]
+     (add-data [\"mice\" 6 \"cats\" 2 \"dogs\" 0])"
+  [coll & {:keys [expt instant]
+             :or {expt active-expt
+                  instant @active-time}}]
   (assert (even? (count coll))
           "Mismatched number of variables and values. Double check call")
   (doall (map (partial apply add-datum)
-              (partition 2 coll)))
+              (map #(conj (vec %) :expt expt :instant instant)
+                   (partition 2 coll))))
   (display-vars))
+
+;;;;
+(defn tag-vars
+  "Adds tag to each var in collection in an experiment."
+  [tag vars & {:keys [expt]
+                 :or {expt active-expt}}]
+  (for [v vars]
+    (swap! expt 
+           update-in 
+           [(str->key v) :tags]
+           #(conj % (str->key tag)))))
 
 ;; desperately needs rewriting, good lord
 (defn display-vars
--- src/familiar/time.clj
+++ src/familiar/time.clj
@@ -1,15 +1,14 @@
 (defn later [a b]
-    (apply > (map to-long [a b])))
+  (apply > (map to-long [a b])))
 (def inst-map (sorted-map-by later))
 
 (def time-form    (formatters :date-time))
 (def unparse-time (partial unparse time-form))
 (def parse-time   (partial parse time-form))
 (defn present []
-    (unparse-time (local-now)))
-(def active-time  (atom (present)))
+  (unparse-time (local-now)))
+(def active-time (atom (present)))
 
 (defn chop-time [instant form]
-  (->> instant
-    (unparse (formatters form))
-    (parse (formatters form))))
+  (->> (parse-time instant)
+       (unparse (formatters form))))
